17f0a3068ab326d3dcb9ca1787daf97e2615b32b
Khaleelur-Rahman <khaleelrrahman2002@gmail.com>
2023-07-05 02:05:47 +0800

    Edited home page to align buttons to center of page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,10 +13,14 @@ function Home() {
 
     return (
         <div className="home">
-            Welcome to your very own book management system, where you can add books that interest you to read in the future or add books that you have already read with your own customized review. Sign in or register to better manage your book world!!
+            <div className="home-title">
+                Welcome to your very own book management system, where you can add books that interest you to read in the future or add books that you have already read with your own customized review. Sign in or register to better manage your book world!!
+            </div>
             <br />
-            <div className="login-home-button" onClick={handleLogin}>Login</div>
-            <div className="register-home-button" onClick={handleRegister}>Register</div>
+            <div className="home-page-buttons">
+                <div className="login-home-button" onClick={handleLogin}>Login</div>
+                <div className="register-home-button" onClick={handleRegister}>Register</div>
+            </div>
         </div>
     )
 }
