48183d35fdee019c79681ed2efd63d4c41f4660b
Khaleelur-Rahman <khaleelrrahman2002@gmail.com>
2023-07-22 22:09:25 +0800

    Typo in backend files

--- src/backend/firebase-config.js
+++ src/backend/firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_iD,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
