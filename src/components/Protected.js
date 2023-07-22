e383f97010205d06a94892ab569275a9ad2ab373
Khaleelur-Rahman <khaleelrrahman2002@gmail.com>
2023-07-23 00:55:28 +0800

    added protected.js to handle user login and logout

---
+++ src/components/Protected.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function Protected ({isSignedIn, children}) {
+    if (isSignedIn) {
+        return children;
+    } else {
+        return <Navigate to="/" replace />;
+    }
+}
+
+export default Protected;
