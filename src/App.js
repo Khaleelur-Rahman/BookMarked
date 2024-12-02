3862c268cd48375cdc209b08be38040ed043ba7f
Khaleelur <khaleelrrahman2002@gmail.com>
2024-12-03 02:04:55 +0800

    refactor App.js file to simplify

--- src/App.js
+++ src/App.js
@@ -9,35 +9,14 @@ import BookForm from "./pages/BookForm";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import "./styles/index.css";
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Protected from "./components/Protected";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setUser(null);
-    setToken(null);
-  };
-
-  if (isLoggedIn) {
-    return (
-      <div className="App">
-        {/*Handles logout*/}
-        <Navbar />
-        <Protected user={user} token={token} handleLogout={handleLogout} />
-      </div>
-    );
-  }
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        {/*Various routes used in the project*/}
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
