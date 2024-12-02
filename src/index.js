bb1dd1b6320919bcd70a591513fc635bd7bcc84e
Khaleelur <khaleelrrahman2002@gmail.com>
2024-12-03 02:05:24 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify"; //Displays popup notificationas
+import { ToastContainer } from "react-toastify"; //For popup notifications
 import "react-toastify/dist/ReactToastify.css";
 
 const rootElement = document.getElementById("root");
