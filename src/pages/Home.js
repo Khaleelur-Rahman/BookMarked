94bf8e7918334cf82622a7ea1ce6d4c2d22c7de1
Khaleelur-Rahman <111182931+Khaleelur-Rahman@users.noreply.github.com>
2024-12-06 04:29:06 +0800

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,64 @@
+import LoginAndRegisterHomeButtons from "../components/LoginAndRegisterHomeButtons";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useMemo, Suspense } from "react";
+import useUserLoggedIn from "../hooks/custom-hooks/useUserLoggedIn";
+
+const Home = () => {
+  const user = useUserLoggedIn();
+
+  const homePageContent = useMemo(() => {
+    if (user === null) {
+      return <LoadingSpinner />;
+    } else if (user !== null) {
+      return (
+        <div className="flex justify-center items-center my-20 font-bold text-2xl text-center">
+          Welcome back {user?.displayName}!
+        </div>
+      );
+    } else {
+      return <LoginAndRegisterHomeButtons />;
+    }
+  }, [user]);
+
+  return (
+    <div className="flex items-center justify-center mt-10 mb-12">
+      <section className="h-screen">
+        <div className="flex items-center justify-center">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+              <img
+                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="w-full"
+                alt="BookMarked webapp icon"
+              />
+            </div>
+
+            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+              <div className="flex flex-col items-center justify-center">
+                <div className="font-urban text-lg font-lg text-center">
+                  Welcome to BookMarked: Where Your Book World Comes Alive!
+                </div>
+
+                <div className="font-urban text-lg font-lg text-center">
+                  Discover, Collect, and Review Your Literary Adventures!
+                </div>
+
+                <div className="font-urban text-lg font-lg text-center">
+                  Unlock the Magic of Books: Register or Login Now!
+                </div>
+              </div>
+
+              <Suspense fallback={<LoadingSpinner />}>
+                <div className="text-center lg:text-left my-20 flex flex-row items-center justify-center">
+                  {homePageContent}
+                </div>
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
