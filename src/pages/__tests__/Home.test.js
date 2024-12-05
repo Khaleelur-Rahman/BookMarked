0013158bb6d7ca3706d99777966aedfefc78ee8f
Khaleelur-Rahman <111182931+Khaleelur-Rahman@users.noreply.github.com>
2024-12-06 04:30:43 +0800

    Create Home.test.js

---
+++ src/pages/__tests__/Home.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../Home.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
+test("Renders Home page with welcome text", () => {
+  render(
+    <Router>
+      <Home />
+    </Router>
+  );
+
+  const firstStatement = screen.getByText(
+    /Welcome to BookMarked: Where Your Book World Comes Alive!/i
+  );
+  const secondStatement = screen.getByText(
+    /Discover, Collect, and Review Your Literary Adventures!/i
+  );
+  const thirdStatement = screen.getByText(
+    /Unlock the Magic of Books: Register or Login Now!/i
+  );
+  expect(firstStatement).toBeTruthy();
+  expect(secondStatement).toBeTruthy();
+  expect(thirdStatement).toBeTruthy();
+});
